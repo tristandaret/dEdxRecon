f52bcbd500eb1071d86b394330a08ce6a5b194ce
Tristan Daret <tristan.daret.phd@gmail.com>
2024-07-02 15:04:44 +0200

    alignment

--- Uploader/inc/Uploader_MockUp_V2.h
+++ Uploader/inc/Uploader_MockUp_V2.h
@@ -5,118 +5,114 @@
 #include "Uploader.h"
 
 /////////////////////////////////////////////////////////////
-class Uploader_MockUp_V2 : public Uploader  {
+class Uploader_MockUp_V2 : public Uploader	{
 public:
-   /** Constructor */
-   Uploader_MockUp_V2(
-            const std::string& SampleFile                 ,
-            Model_ReadOutGeometry* pModel_ReadOutGeometry ,
-            Model_Electronics*     pModel_Electronics     ,
-            Model_Charge1D*        pModel_Charge1D        
-   );
-   virtual ~Uploader_MockUp_V2();
-            
-   virtual Event* GiveMe_Event(
-                               const int& iEvent, 
-                               const int& ModuleNber_Input, 
-                               const int& Data_to_Use,
-                               const int& CloseWF
-                              ) ;
+	/** Constructor */
+	Uploader_MockUp_V2(
+			const std::string& SampleFile				 ,
+			Model_ReadOutGeometry* pModel_ReadOutGeometry ,
+			Model_Electronics*	 pModel_Electronics	 ,
+			Model_Charge1D*		pModel_Charge1D		
+	);
+	virtual ~Uploader_MockUp_V2();
+			
+	virtual Event* GiveMe_Event(
+								const int& iEvent, 
+								const int& ModuleNber_Input, 
+								const int& Data_to_Use,
+								const int& CloseWF
+								) ;
 
-   virtual Event* GiveMe_Event(
-                               const int& iEvent, 
-                               const int& ModuleNber_Input, 
-                               const int& Data_to_Use
-                              ) ;
-   
-   void SetBeforeMerging(const int& i_SetBeforeMerging) ;
-   void Setwap_XY(const int& i_Swap_XY) ;
+	virtual Event* GiveMe_Event(
+								const int& iEvent, 
+								const int& ModuleNber_Input, 
+								const int& Data_to_Use
+								) ;
+	
+	void SetBeforeMerging(const int& i_SetBeforeMerging) ;
+	void Setwap_XY(const int& i_Swap_XY) ;
 
 //------------------------------Data Members-----------------------//
 public:
-   Int_t           fCurrent;
+	Int_t										fCurrent;
 
-   Int_t           ev;
-   Int_t           track;
-   ULong64_t       date; // Added since code update contains the date of the events !
-   Bool_t          beforeMerging; // Added since code update tells if this track has been saved before or after merging algorithm
-   Double_t        dEdx;
-   Double_t        angle_yz;
-   Double_t        angle_xy;
-   Int_t           rob_clusters;
-   Double_t        quality;
-   Double_t        mom;
-   Double_t        sina;
-   Double_t        offset;
-   Int_t           max_mult;
-   Double_t        mean_mult;
-   std::vector<int>     *multiplicity;
-   std::vector<int>     *charge;
-   std::vector<double>  *residual;
-   std::vector<double>  *residual_corr;
-   std::vector<std::vector<double> > *dx;
-   std::vector<std::vector<double> > *qfrac;
-   std::vector<std::vector<int> > *time;
-   std::vector<double>  *clust_pos;
-   std::vector<double>  *clust_pos_err;
-   std::vector<double>  *track_pos;
-   std::vector<int>     *module;
-   std::vector<std::vector<int> > *pad_charge;
-   std::vector<std::vector<int> > *pad_time;
-   std::vector<std::vector<int> > *wf_width;
-   std::vector<std::vector<int> > *wf_fwhm;
-   std::vector<std::vector<int> > *pad_x;
-   std::vector<std::vector<int> > *pad_y;
-   std::vector<std::vector<std::vector<int> > > *pad_wf_q;
+	Int_t										ev;
+	Int_t										track;
+	ULong64_t									date; 			// Added since code update contains the date of the events !
+	Bool_t										beforeMerging; 	// Added since code update tells if this track has been saved before or after merging algorithm
+	Double_t									dEdx;
+	Double_t									angle_yz;
+	Double_t									angle_xy;
+	Int_t										rob_clusters;
+	Double_t									quality;
+	Double_t									mom;
+	Double_t									sina;
+	Double_t									offset;
+	Int_t										max_mult;
+	Double_t									mean_mult;
+	std::vector<int>	 						*multiplicity;
+	std::vector<int>	 						*charge;
+	std::vector<double>							*residual;
+	std::vector<double>							*residual_corr;
+	std::vector<std::vector<double>> 			*dx;
+	std::vector<std::vector<double>> 			*qfrac;
+	std::vector<std::vector<int>>	 			*time;
+	std::vector<double>							*clust_pos;
+	std::vector<double>							*clust_pos_err;
+	std::vector<double>							*track_pos;
+	std::vector<int>	 						*module;
+	std::vector<std::vector<int>>				*pad_charge;
+	std::vector<std::vector<int>>				*pad_time;
+	std::vector<std::vector<int>>				*wf_width;
+	std::vector<std::vector<int>>				*wf_fwhm;
+	std::vector<std::vector<int>>				*pad_x;
+	std::vector<std::vector<int>>				*pad_y;
+	std::vector<std::vector<std::vector<int>>>	*pad_wf_q;
 
-   // List of branches
-   TBranch        *b_ev;   //!
-   TBranch        *b_track;   //!
-   TBranch        *b_date;   //!
-   TBranch        *b_beforeMerging;   //!
-   TBranch        *b_dEdx;   //!
-   TBranch        *b_angle_yz;   //!
-   TBranch        *b_angle_xy;   //!
-   TBranch        *b_rob_clusters;   //!
-   TBranch        *b_quality;   //!
-   TBranch        *b_mom;   //!
-   TBranch        *b_sina;   //!
-   TBranch        *b_offset;   //!
-   TBranch        *b_max_mult;   //!
-   TBranch        *b_mean_mult;   //!
-   TBranch        *b_multiplicity;   //!
-   TBranch        *b_charge;   //!
-   TBranch        *b_residual;   //!
-   TBranch        *b_residual_corr;   //!
-   TBranch        *b_dx;   //!
-   TBranch        *b_qfrac;   //!
-   TBranch        *b_time;   //!
-   TBranch        *b_clust_pos;   //!
-   TBranch        *b_clust_pos_err;   //!
-   TBranch        *b_track_pos;   //!
-   TBranch        *b_module;   //!
-   TBranch        *b_pad_charge;   //!
-   TBranch        *b_pad_time;   //!
-   TBranch        *b_wf_width;   //!
-   TBranch        *b_wf_fwhm;   //!
-   TBranch        *b_pad_x;   //!
-   TBranch        *b_pad_y;   //!
-   TBranch        *b_pad_wf_q;   //!
+	// List of branches
+	TBranch		*b_ev;
+	TBranch		*b_track;
+	TBranch		*b_date;
+	TBranch		*b_beforeMerging;
+	TBranch		*b_dEdx;
+	TBranch		*b_angle_yz;
+	TBranch		*b_angle_xy;
+	TBranch		*b_rob_clusters;
+	TBranch		*b_quality;
+	TBranch		*b_mom;
+	TBranch		*b_sina;
+	TBranch		*b_offset;
+	TBranch		*b_max_mult;
+	TBranch		*b_mean_mult;
+	TBranch		*b_multiplicity;
+	TBranch		*b_charge;
+	TBranch		*b_residual;
+	TBranch		*b_residual_corr;
+	TBranch		*b_dx;
+	TBranch		*b_qfrac;
+	TBranch		*b_time;
+	TBranch		*b_clust_pos;
+	TBranch		*b_clust_pos_err;
+	TBranch		*b_track_pos;
+	TBranch		*b_module;
+	TBranch		*b_pad_charge;
+	TBranch		*b_pad_time;
+	TBranch		*b_wf_width;
+	TBranch		*b_wf_fwhm;
+	TBranch		*b_pad_x;
+	TBranch		*b_pad_y;
+	TBranch		*b_pad_wf_q;
 
-   void GetEntry(Long64_t entry);
-   void Init();
+	void Init();
 
 private:
 
-   int m_BeforeMerging ;
-   int m_Swap_XY ;
-
-   int m_Nx ;
-   int m_Ny ;
+	int 		m_BeforeMerging ;
+	int 		m_Swap_XY ;
+	int 		m_Nx ;
+	int 		m_Ny ;
 
 };
-
-
 
 #endif
 
--- Uploader/src/Uploader_MockUp_V1.cxx
+++ Uploader/src/Uploader_MockUp_V1.cxx
@@ -1,6 +1,6 @@
-#include "Uploader_MockUp_V2.h"
+#include "Uploader_MockUp_V1.h"
 
-Uploader_MockUp_V2::Uploader_MockUp_V2(
+Uploader_CERN22::Uploader_CERN22(
             const std::string& SampleFile                 ,
             Model_ReadOutGeometry* pModel_ReadOutGeometry ,
             Model_Electronics*     pModel_Electronics     ,
@@ -12,19 +12,17 @@ Uploader_MockUp_V2::Uploader_MockUp_V2(
                                    pModel_Charge1D        
 )
 {  
-  m_Nx = pModel_ReadOutGeometry->Get_Nx();
-  m_Ny = pModel_ReadOutGeometry->Get_Ny();
-  
-  Init();
-
-  m_BeforeMerging = 0 ;
-  m_Swap_XY = 0 ;
+  std::cout << std::endl ;
+  std::cout << "  Uploader_CERN22::Uploader_CERN22 "<< std::endl ;
 }
 
 
-Uploader_MockUp_V2::~Uploader_MockUp_V2(){}
+Uploader_CERN22::~Uploader_CERN22()
+{
+//std::cout << " In Uploader_CERN22::~Uploader_CERN22() " << std::endl ;
+}
 
-Event* Uploader_MockUp_V2::GiveMe_Event(
+Event* Uploader_CERN22::GiveMe_Event(
                                const int& iEvent, 
                                const int& ModuleNber_Input, 
                                const int& Data_to_Use
@@ -32,7 +30,7 @@ Event* Uploader_MockUp_V2::GiveMe_Event(
   return GiveMe_Event(iEvent, ModuleNber_Input, Data_to_Use, 1) ;
 }
 
-Event* Uploader_MockUp_V2::GiveMe_Event(
+Event* Uploader_CERN22::GiveMe_Event(
                                const int& iEvent, 
                                const int& ModuleNber_Input, 
                                const int& Data_to_Use,
@@ -40,22 +38,79 @@ Event* Uploader_MockUp_V2::GiveMe_Event(
 ){
   if (iEvent >= Get_NberOfEvent() ) return 0 ;
  
-  if (!p_TTree) return 0;
-  fCurrent = iEvent;
-  
-//Set off all branches but beforeMerging  
-  p_TTree->SetBranchStatus("*",0);
-  p_TTree->SetBranchStatus("beforeMerging",1);
-  p_TTree->GetEntry(iEvent);
- 
-//Keep only the entries corresponding to before or after merging 
+//Declaration of leaf types
+  Int_t           ev ;
+  Double_t        dEdx ;
+  Double_t        angle_yz ;
+  Double_t        angle_xy ;
+  Int_t           rob_clusters ;
+  Double_t        quality ;
+  Double_t        mom  ;
+  Double_t        sina ;
+  Double_t        offset ;
+  Int_t           max_mult ;
+  Double_t        mean_mult ;
+  Int_t           multiplicity[200];
+  Int_t           charge[200];
+  Double_t        residual[200];
+  Double_t        residual_corr[200];
+  Double_t        dx[200][10];
+  Double_t        qfrac[200][10];
+  Int_t           time[200][10];
+  Double_t        clust_pos[200];
+  Double_t        clust_pos_err[200];
+  Double_t        track_pos[200];
+  Int_t           module[200];
+  Int_t           pad_charge[200][10];
+  Int_t           pad_time[200][10];
+  Int_t           wf_width[200][10];
+  Int_t           wf_fwhm[200][10];
+  Int_t           pad_x[200][10];
+  Int_t           pad_y[200][10];
+  Int_t           pad_wf_q[200][10][520];
 
-  if (beforeMerging!=m_BeforeMerging) return 0 ;
-  
-//The Event is OK, Set on all branches 
-  p_TTree->SetBranchStatus("*",1); 
-  p_TTree->GetEntry(iEvent);
 
+//Set branch addresses and branch pointers
+  p_TTree->SetBranchAddress("ev", &ev);
+  p_TTree->SetBranchAddress("dEdx", &dEdx);
+  p_TTree->SetBranchAddress("angle_yz", &angle_yz);
+  p_TTree->SetBranchAddress("angle_xy", &angle_xy);
+  p_TTree->SetBranchAddress("rob_clusters", &rob_clusters);
+  p_TTree->SetBranchAddress("quality", &quality);
+  p_TTree->SetBranchAddress("mom", &mom);
+  p_TTree->SetBranchAddress("sina", &sina);
+  p_TTree->SetBranchAddress("offset", &offset);
+  p_TTree->SetBranchAddress("max_mult", &max_mult);
+  p_TTree->SetBranchAddress("mean_mult", &mean_mult);
+  p_TTree->SetBranchAddress("multiplicity", &multiplicity);
+  p_TTree->SetBranchAddress("charge", &charge);
+  p_TTree->SetBranchAddress("residual", &residual);
+  p_TTree->SetBranchAddress("residual_corr", &residual_corr);
+  p_TTree->SetBranchAddress("dx", &dx);
+  p_TTree->SetBranchAddress("qfrac", &qfrac);
+  p_TTree->SetBranchAddress("time", &time);
+  p_TTree->SetBranchAddress("clust_pos", &clust_pos);
+  p_TTree->SetBranchAddress("clust_pos_err", &clust_pos_err);
+  p_TTree->SetBranchAddress("track_pos", &track_pos);
+  p_TTree->SetBranchAddress("module", &module);
+  p_TTree->SetBranchAddress("pad_charge", &pad_charge);
+  p_TTree->SetBranchAddress("pad_time", &pad_time);
+  p_TTree->SetBranchAddress("wf_width", &wf_width);
+  p_TTree->SetBranchAddress("wf_fwhm", &wf_fwhm);
+  p_TTree->SetBranchAddress("pad_x", &pad_x);
+  p_TTree->SetBranchAddress("pad_y", &pad_y);
+  p_TTree->SetBranchAddress("pad_wf_q", &pad_wf_q);                                   
+
+// 
+  int iPad_Max     =  10 ; 
+  int iCluster_Max = 200 ; 
+  int iTimeBin_Max = 510 ;
+  int iTimeBin_Min =   0 ;
+
+//
+  p_TTree->GetEntry(iEvent);
+  
+//  
   Event*  pEvent = new Event( 
                              ev     ,
                              iEvent ,
@@ -77,17 +132,16 @@ Event* Uploader_MockUp_V2::GiveMe_Event(
                             ); 
     V_pModule.push_back( pModule ) ;
   }
-  
-  int iCluster_Max = module->size() ;
+     
   for (Int_t iCluster = 0; iCluster < iCluster_Max; iCluster++){
   
 //  SetModule
-    int ModuleNber_Cur = module->at(iCluster);
+    int ModuleNber_Cur = module[iCluster] ;  
     if ( ModuleNber_Input!=-1 && ModuleNber_Cur != ModuleNber_Input ) continue ;
 
     Module* pModule = V_pModule[ModuleNber_Cur] ;
      
-    if(charge->at(iCluster) < 0) continue;
+    if(charge[iCluster] < 0) continue;
     Cluster* pCluster = new Cluster(
                                     ev     ,
                                     iEvent ,
@@ -97,34 +151,13 @@ Event* Uploader_MockUp_V2::GiveMe_Event(
     // It seems that there are clusters (cluster charge >=0 but with no pad (charge <0 for all pads)
     // check and store the cluster only if there are pads in it
     int NberOfPads = 0 ; 
-    int iPad_Max= pad_charge->at(iCluster).size() ;
     for (Int_t iPad = 0; iPad < iPad_Max; iPad++){
-      if(pad_charge->at(iCluster).at(iPad)<0)continue;
+      if(pad_charge[iCluster][iPad]<0)continue;
       NberOfPads += 1 ;
       
-      int iX_Get         = pad_x->at(iCluster).at(iPad); ;
-      int iY_Get         = pad_y->at(iCluster).at(iPad); ;
+      int iX         = pad_x[iCluster][iPad] ;
+      int iY         = pad_y[iCluster][iPad] ;
                       
-      int iX_Set         = iX_Get ;
-      int iY_Set         = iY_Get ;
-      
-//    Swap X and Y axes
-      if (m_Swap_XY==1){                      
-        iX_Set = (m_Ny-1) - iY_Get ;
-        iY_Set = iX_Get             ;
-      }
-
-      if (iX_Set < 0 || iX_Set >= m_Nx || iY_Set < 0 || iY_Set >= m_Ny ){
-        std::cout << " Uploader_MockUp_V2::GiveMe_Event "
-                  << " Wrong  "  
-                  << " iCluster= " << iCluster
-                  << " iX_Set= " << iX_Set
-                  << " iY_Set= " << iY_Set
-                  << std::endl; 
-        abort(); 
-      }
-      
-                            
       Pad* pPad = new Pad (p_Model_ReadOutGeometry , 
                            p_Model_Electronics     , 
                            p_Model_Charge1D        ,
@@ -132,28 +165,26 @@ Event* Uploader_MockUp_V2::GiveMe_Event(
                            ev     ,
                            iEvent ,
                            ModuleNber_Cur ,
-                           iX_Set ,iY_Set ); 
+                           iX ,iY ); 
                                            
-      pPad->Set_AMax(pad_charge->at(iCluster).at(iPad));
-      pPad->Set_TMax(pad_time  ->at(iCluster).at(iPad));
+      pPad->Set_AMax(pad_charge[iCluster][iPad]);
+      pPad->Set_TMax(pad_time  [iCluster][iPad]);
 
       pPad->Clear_ADC() ;
-      int iTimeBin_Min = 0 ;
-      int iTimeBin_Max = pad_wf_q->at(iCluster).at(iPad).size() ;
       for (Int_t iTimeBin = iTimeBin_Min; iTimeBin < iTimeBin_Max; iTimeBin++){
         int iTimeBinProper = iTimeBin ;
-        int ADCProper      = pad_wf_q->at(iCluster)[iPad][iTimeBin] ;
-        if ( ADCProper < -250 ) {
-          ADCProper = 0 ;
-        }
+        int ADCProper      = pad_wf_q[iCluster][iPad][iTimeBin] ;
+        if ( ADCProper < -250 ) ADCProper = 0 ;
         if ( iTimeBinProper < iTimeBin_Max ) pPad->Set_ADC(iTimeBinProper,ADCProper ) ;
       }
       if(CloseWF == 1) pPad->Wf_DoClosure() ;
       
       pPad->Set_Data_2Use(Data_to_Use) ;
-
+              
       pModule->Add_Pad(pPad ) ;
+      
       pCluster->Add_Pad(pPad) ;
+
     }
     if (NberOfPads!=0){
       pCluster->DoClosure() ;
@@ -168,7 +199,7 @@ Event* Uploader_MockUp_V2::GiveMe_Event(
     if (NClusters!=0){
       pEvent->Add_Module(pModule) ;
     }else{
-      delete pModule ; pModule = 0 ;
+      delete pModule ;
     }
   }
   int NberOFModule = pEvent->Get_NberOfModule() ;
@@ -179,73 +210,4 @@ Event* Uploader_MockUp_V2::GiveMe_Event(
   }
 
   return pEvent ;
-}
-
-void Uploader_MockUp_V2::SetBeforeMerging(const int& i_SetBeforeMerging)
-{
-  m_BeforeMerging = i_SetBeforeMerging;
-}
-
-void Uploader_MockUp_V2::Setwap_XY(const int& i_Swap_XY)
-{
-  m_Swap_XY = i_Swap_XY;
-}
-
-void Uploader_MockUp_V2::Init()
-{
-   multiplicity = 0;
-   charge = 0;
-   residual = 0;
-   residual_corr = 0;
-   dx = 0;
-   qfrac = 0;
-   time = 0;
-   clust_pos = 0;
-   clust_pos_err = 0;
-   track_pos = 0;
-   module = 0;
-   pad_charge = 0;
-   pad_time = 0;
-   wf_width = 0;
-   wf_fwhm = 0;
-   pad_x = 0;
-   pad_y = 0;
-   pad_wf_q = 0;
-   // Set branch addresses and branch pointers
-
-   fCurrent = -1;
-   p_TTree->SetMakeClass(1);
-
-   p_TTree->SetBranchAddress("ev", &ev, &b_ev);
-   p_TTree->SetBranchAddress("track", &track, &b_track);
-   p_TTree->SetBranchAddress("date", &date, &b_date);
-   p_TTree->SetBranchAddress("beforeMerging", &beforeMerging, &b_beforeMerging);
-   p_TTree->SetBranchAddress("dEdx", &dEdx, &b_dEdx);
-   p_TTree->SetBranchAddress("angle_yz", &angle_yz, &b_angle_yz);
-   p_TTree->SetBranchAddress("angle_xy", &angle_xy, &b_angle_xy);
-   p_TTree->SetBranchAddress("rob_clusters", &rob_clusters, &b_rob_clusters);
-   p_TTree->SetBranchAddress("quality", &quality, &b_quality);
-   p_TTree->SetBranchAddress("mom", &mom, &b_mom);
-   p_TTree->SetBranchAddress("sina", &sina, &b_sina);
-   p_TTree->SetBranchAddress("offset", &offset, &b_offset);
-   p_TTree->SetBranchAddress("max_mult", &max_mult, &b_max_mult);
-   p_TTree->SetBranchAddress("mean_mult", &mean_mult, &b_mean_mult);
-   p_TTree->SetBranchAddress("multiplicity", &multiplicity, &b_multiplicity);
-   p_TTree->SetBranchAddress("charge", &charge, &b_charge);
-   p_TTree->SetBranchAddress("residual", &residual, &b_residual);
-   p_TTree->SetBranchAddress("residual_corr", &residual_corr, &b_residual_corr);
-   p_TTree->SetBranchAddress("dx", &dx, &b_dx);
-   p_TTree->SetBranchAddress("qfrac", &qfrac, &b_qfrac);
-   p_TTree->SetBranchAddress("time", &time, &b_time);
-   p_TTree->SetBranchAddress("clust_pos", &clust_pos, &b_clust_pos);
-   p_TTree->SetBranchAddress("clust_pos_err", &clust_pos_err, &b_clust_pos_err);
-   p_TTree->SetBranchAddress("track_pos", &track_pos, &b_track_pos);
-   p_TTree->SetBranchAddress("module", &module, &b_module);
-   p_TTree->SetBranchAddress("pad_charge", &pad_charge, &b_pad_charge);
-   p_TTree->SetBranchAddress("pad_time", &pad_time, &b_pad_time);
-   p_TTree->SetBranchAddress("wf_width", &wf_width, &b_wf_width);
-   p_TTree->SetBranchAddress("wf_fwhm", &wf_fwhm, &b_wf_fwhm);
-   p_TTree->SetBranchAddress("pad_x", &pad_x, &b_pad_x);
-   p_TTree->SetBranchAddress("pad_y", &pad_y, &b_pad_y);
-   p_TTree->SetBranchAddress("pad_wf_q", &pad_wf_q, &b_pad_wf_q);
 }
